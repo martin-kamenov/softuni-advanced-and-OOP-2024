2559f62ed23940d1ee06d1002c2ca9ca669c2472
marto-92 <mar7in.kamenov@gmail.com>
2024-07-22 21:06:24 +0300

    added more solutions

---
+++ OOP/08_iterators_and_generators_exercise/06_fibonacci_generator.py
@@ -0,0 +1,16 @@
+def fibonacci():
+    num1, num2 = 0, 1
+
+    while True:
+        yield num1
+
+        num1, num2 = num2, num1 + num2
+
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
+generator = fibonacci()
+for i in range(1):
+    print(next(generator))
